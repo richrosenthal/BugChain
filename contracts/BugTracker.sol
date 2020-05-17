bb62befa342a2da941b0bf90e35f3520138ca0a9
richrosenthal <rrosenthal2013@gmail.com>
2020-05-17 02:23:13 -0400

    flesh out bug task model

--- contracts/BugTracker.sol
+++ contracts/BugTracker.sol
@@ -3,4 +3,22 @@ pragma solidity ^0.5.0;
 contract BugTracker {
 	uint public taskCount = 0;
 
+	struct Task {
+		uint id;
+		string title;
+		string content;
+		bool completed;
+	}
+
+	mapping(uint => Task) public tasks;
+
+	constructor() public {
+		createTask("First Bug", "All your base are belong to us");
+	}
+
+	function createTask(string memory _title, string memory _content) public {
+		taskCount ++;
+		tasks[taskCount] = Task(taskCount, _title, _content, false);
+	}
+
 }
