9d8b528daf2032c24250a2b901061a826c0e9bea
richrosenthal <rrosenthal2013@gmail.com>
2020-05-17 03:50:11 -0400

    working code

--- contracts/BugTracker.sol
+++ contracts/BugTracker.sol
@@ -12,13 +12,22 @@ contract BugTracker {
 
 	mapping(uint => Task) public tasks;
 
+	event TaskCreated(
+		uint id,
+		string title,
+		string content,
+		bool completed
+		);
+
 	constructor() public {
-		createTask("First Bug", "All your base are belong to us");
+		createTask("First Buggg", "All your base are belong to usggg");
 	}
 
 	function createTask(string memory _title, string memory _content) public {
 		taskCount ++;
 		tasks[taskCount] = Task(taskCount, _title, _content, false);
+		emit TaskCreated(taskCount, _title, _content, false);
+
 	}
 
 }
